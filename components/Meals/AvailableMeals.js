1bcbacc0dfc2e682cc569d808c905f70cd752c35
Dipankar Basak <143477069+Bassakdip@users.noreply.github.com>
2024-03-31 08:58:39 +0530

    Update AvailableMeals.js

--- components/Meals/AvailableMeals.js
+++ components/Meals/AvailableMeals.js
@@ -1,36 +1,37 @@
 import React from 'react'
 import classes from './AvailableMeals.module.css'
 import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const DummyMeals = [
     {
         id: 'm1',
         name: 'Mutton Tikiya',
         describtion: 'It is a flavorful Indian street food, comprising spiced mutton(6 pc)',
-        price: 175
+        price: 177.75
     },
     {
         id: 'm2',
         name: 'Chicken Biryani',
         describtion: 'Chicken (1 Pc) + Aloo (1 Pc) + Rice In 1000 Ml Container',
-        price: 235
+        price: 235.99
     },
     {
         id: 'm3',
         name: 'Kashmiri Pulao',
         describtion: 'A traditional kashmiri dish, this mildly spicy and flavour-loaded rice dish',
-        price: 159
+        price: 169.69
     },
     {
         id: 'm4',
         name: 'Butter Chicken ',
         describtion: 'One of the most popular Punjabi style chicken dishes in the world ,',
-        price: 299
+        price: 275.89
     }
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DummyMeals.map((meal) => <li>{meal.name}</li>);
+    const mealsList = DummyMeals.map((meal) => <MealItem key={meal.id} name={meal.name} describtion={meal.describtion} price={meal.price}/>);
     return (
         <section className={classes.meals}>
             <Card>
